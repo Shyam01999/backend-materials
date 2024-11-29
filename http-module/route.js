a66b14c08a8f6065313b28b654fc8a5ea504764a
shyam1998 <shyamsundarsahoo1998@gmail.com>
2024-11-29 23:08:13 +0530

    http module and route in node js

---
+++ http-module/route.js
@@ -0,0 +1,22 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    if (url === '/') {
+        res.writeHead(200, { "Content-Type": "text-plain" });
+        res.end("Home page")
+    } else if (url === '/project') {
+        res.writeHead(200, { "Content-Type": "text-plain" });
+        res.end("Project")
+    } else {
+        res.writeHead(404, { "Content-Type": "text-plain" });
+        res.end("This url does does not exist")
+    }
+   
+})
+
+const PORT = 5000;
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
