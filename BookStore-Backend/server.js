37439dade4ea840529031face0dff81c1f0f2db4
shyam1998 <shyamsundarsahoo1998@gmail.com>
2025-04-13 22:36:43 +0530

    crup api

--- BookStore-Backend/server.js
+++ BookStore-Backend/server.js
@@ -1,20 +1,21 @@
 require("dotenv").config();
 const express = require("express");
 const connectToDb = require("./database/db");
+const bookRoute = require("./routes/book.routes")
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 //connect to database
 connectToDb();
 
 //middlware
-// app.use(express.json());
+app.use(express.json());
 
 //routes
-// app.use("/api/user",);
+app.use("/api/book", bookRoute);
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
 
