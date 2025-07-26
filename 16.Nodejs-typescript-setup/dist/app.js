f43f3424da291031a415d4138360509c19b1bfd0
shyam01999 <shyamsundarsahoo1998@gmail.com>
2025-07-26 22:04:23 +0530

    type script project

---
+++ 16.Nodejs-typescript-setup/dist/app.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Hello, from node js file");
+function getName(name) {
+    return name;
+}
+console.log(getName("Shyam"));
---
+++ 16.Nodejs-typescript-setup/src/app.ts
@@ -0,0 +1,7 @@
+console.log("Hello, from node js file");
+
+function getName(name : string){
+    return name;
+}
+
+console.log(getName("Shyam"));
