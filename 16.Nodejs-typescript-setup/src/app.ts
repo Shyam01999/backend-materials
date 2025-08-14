933ec4eb8ed6f51145d3f03fc78f2a85988f0027
shyam01999 <shyamsundarsahoo1998@gmail.com>
2025-08-15 00:02:09 +0530

    main

--- 16.Nodejs-typescript-setup/src/app.ts
+++ 16.Nodejs-typescript-setup/src/app.ts
@@ -36,9 +36,20 @@ interface User {
 }
 
 app.post("/user", (req: Request<{}, {}, User>, res: Response) => {
-    const {name, email} = req.body;
+  const { name, email } = req.body;
 
-    
+  res.status(200).json({
+    message: `User ${name} with email ${email} created successfully!`,
+  });
+});
+
+
+app.post("/user/:id", (req: Request<{id: string}>, res: Response) => {
+  const { id } = req.params;
+
+  res.status(200).json({
+    userid : id,
+  });
 });
 
 
