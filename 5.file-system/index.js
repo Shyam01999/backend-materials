6499a5de871720e49f73bf428421b948965c43b6
shyam01999 <shyamsundarsahoo1998@gmail.com>
2026-02-10 21:56:36 +0530

    new eample added

--- 5.file-system/index.js
+++ 5.file-system/index.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const dataFolder = path.join(__dirname, "data");
+console.log("dataFolder", dataFolder);
 
 if (!fs.existsSync(dataFolder)) {
     fs.mkdirSync(dataFolder)
@@ -20,3 +21,16 @@ console.log("new content appended")
 
 const readContentFromFile = fs.readFileSync(filePath, "utf-8");
 console.log("file content:", readContentFromFile);
+
+//Example of JS file creation
+// const filePath2 = path.join(dataFolder, 'demo.js');
+
+// fs.writeFileSync(filePath2, 'This is a js file');
+// console.log("Js file created");
+
+// fs.appendFileSync(filePath2, "\n This is a append content into the file");
+// console.log("new content appended")
+
+// const readContentFromJsFile = fs.readFileSync(filePath2, 'utf-8');
+// console.log("Js file content", readContentFromJsFile)
+
